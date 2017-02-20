d318ab753e27f7f5df4b3c6e20bae225757bccfe
vinothsukumaran <vinoth.suku@gmail.com>
2017-02-20 10:34:54 -0600

    First Commit

---
+++ SampleJava/src/StringDemo.java
@@ -0,0 +1,16 @@
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str1 = "Hello";
+		
+		String str2 = " World First";
+		
+		String stradd = str1 + str2;
+		
+		System.out.println(stradd);
+	}
+
+}
